70dbdb2d28255bc15c9839e9abde17b9cf8b7e29
PengJuan-AI <pj2018022187@126.com>
2023-11-05 08:26:27 -0500

    Update

--- home.py
+++ home.py
@@ -33,10 +33,35 @@ def run_UI():
     # Set the page title
     st.header("GabbyGarden 🌱\n \
         Sowing Seeds of Knowledge and Imagination for Kids 💫")
+    st.markdown("""
+                <style>
+                    /* Center the container horizontally */
+                    .image-container {
+                        display: flex;
+                        justify-content: center; /* Center horizontally */
+                    }
+                    
+                    /* Style the images (optional) */
+                    .image-container img {
+                        width: 350px; /* Set the width of each image */
+                        height: auto; /* Maintain the aspect ratio */
+                        margin-top: 30px 
+                    }
+                </style>
+                <div class="image-container">
+                    <img src="app/static/flowers.GIF" width="300" height="200"/>
+                </div>
+                """, unsafe_allow_html=True)
     
     # Sidebar menu
     with st.sidebar:
         st.subheader("About the app")
+        st.markdown("""
+                    With **GabbyGarden AI**, kids can just 
+                    - speak up and listen to stories, 
+                    - making it easy for them to learn and have fun.
+                    It's like having a smart friend 😆 who's always there to explore and learn with them👏🏻👏🏼👏🏼👏🏽👏🏾.
+                    """)
 
 # Application entry point
 if __name__ == "__main__":
---
+++ pages/❓Ask Me.py
@@ -0,0 +1,70 @@
+import streamlit as st
+from streamlit_mic_recorder import speech_to_text
+from dotenv import load_dotenv
+from html_chatbot_template import css, bot_template, user_template
+from APIs.talk import chat_with_child
+from APIs.text2speech import get_speech_from_text
+from APIs.helper import autoplay_audio
+
+def generate_response(conversation_log):
+
+    # Get the response from the chat model for the user query
+    if "system" not in st.session_state:
+        st.session_state["system"] = 0
+    if "chat_hist" not in st.session_state:
+        st.session_state["chat_hist"] = []
+    # for msg in st.session_state.messages:
+    #     st.chat_message(msg["role"]).write(msg["content"])
+        
+    st.session_state.chat_hist.extend(conversation_log)
+    st.write(css, unsafe_allow_html=True)
+    welcome = "Hello! Welcome to GabbyGarden! I am Gab.\n You can ask me any question you like! I'm here to help you learn and understand new things."
+    # st.write(st.session_state.chat_hist)
+    for message in st.session_state.chat_hist:
+        # Check if the message is from the user or the chatbot
+        if message['role'] == 'assistant':
+            # bot message
+            st.write(bot_template.replace(
+                "{{MSG}}", message['content']), unsafe_allow_html=True)
+        elif message['role'] == 'system' and st.session_state.system==0:
+            st.session_state.system = 1
+            st.write(bot_template.replace(
+                "{{MSG}}", welcome), unsafe_allow_html=True)
+        elif message['role'] == 'user':
+            # user message
+            st.write(user_template.replace(
+                "{{MSG}}", message['content']), unsafe_allow_html=True)
+    autoplay_audio("./assets/speech.mp3")  
+                
+def mic():
+    state=st.session_state
+
+    if 'text_received' not in state:
+        state.text_received=[]
+    # if '_last_audio_id' not in state:
+    #     state._last_audio_id = 0
+
+    text=speech_to_text(language='en',start_prompt="Ask Me 😊",
+                        use_container_width=True,just_once=True,key='STT')
+    
+    if text:       
+        state.text_received.append(text)
+        # st.session_state.conversations = get_conversation_chain(text)
+        log = chat_with_child(text)
+        # st.markdown(log)
+        generate_response(log)
+        # autoplay_audio("./assets/speech.mp3")   
+
+mic()
+with st.sidebar:
+    st.subheader("🧸 How to Play with Ask Me?")
+    st.markdown("""
+                 💡 Hey kiddo! Curiosity is fantastic! What can I assist you with?
+                 
+                 💡 Little genius! Ready to unravel some mysteries?
+                 
+                 💡 Hey curious mind! What's the question of the day?
+                 
+                 **JUST CLICK `Ask Me` button and SAY OUTLOUD 📣 you questions**
+                 """)
+    
